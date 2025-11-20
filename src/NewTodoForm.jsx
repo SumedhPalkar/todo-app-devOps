1e4953cdf4c14843c55878f5b9b383af5130578a
sumedh <sumedhpalkar1998@gmail.com>
2025-11-20 22:05:05 +0530

    Initial files

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+
+const NewTodoForm = ({onSubmit}) => {
+
+    const [newItem, setNewItem] = useState("");
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+    
+        if(newItem === "") return
+
+        onSubmit(newItem)
+
+        setNewItem("");
+      }
+
+  return (
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+    </>
+  )
+}
+
+export default NewTodoForm
